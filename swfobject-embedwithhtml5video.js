d38a2804bcdbaf87870193c1dc06f11e367c97b3
Chandler McWilliams <chandler@brysonian.com>
2009-09-18 20:35:18 -0700

    generalizing away from iphone, using better DOM js

--- swfobject-embedwithhtml5video.js
+++ swfobject-embedwithhtml5video.js
@@ -1,15 +1,17 @@
-/*	embedVideoWithIPOS by Chandler McWilliams <http://github.com/brysonian/SWFObject-embedVideoWithIPOS> 
+/*	embedWithHTML5Video by Chandler McWilliams <http://github.com/brysonian/SWFObject-embedWithHTML5Video> 
 */
-swfobject.embedVideoWithIPOS = function(swf, id, width, height, version, express, vars, params, attribs, callback, iphonevars) {
+swfobject.embedWithHTML5Video = function(swf, id, width, height, version, express, vars, params, attribs, callback, videovars) {
 	swfobject.embedSWF(swf, id, width, height, version, express, vars, params, attribs, function(e) {
-		if(!e.success && ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)))) {
-			var w = iphonevars.width?iphonevars.width:'320';
-			var h = iphonevars.height?iphonevars.height:'240';
-			
-			var v = '<video src="'+iphonevars.mp4+'" width="'+w+'" height="'+h+'" ';
-			for(var p in iphonevars) if (p != 'mp4' && p != 'width' && p != 'height') v += p+'="'+iphonevars[p]+'" ';
-			v += ' >Could not display video.</video>';
-			document.getElementById(id).innerHTML = v;
+		var v;
+		if(!e.success && (v = document.createElement('video'))) {
+			var w = videovars.width?videovars.width:'320';
+			var h = videovars.height?videovars.height:'240';
+			v.setAttribute('src', videovars.mp4);
+			v.setAttribute('width', w);
+			v.setAttribute('height', h);
+			for(var p in videovars) if (p != 'mp4' && p != 'width' && p != 'height') v.setAttribute(p, videovars[p]);
+			v.appendChild(document.createTextNode("Could not display video."));
+			document.getElementById(id).appendChild(v);
 		}
 		if (typeof callback == 'function') callback(e);
 	});
